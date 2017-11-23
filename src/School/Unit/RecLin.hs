2764250b43f1203356a431eadef11fc12f1178f4
jorgen.fulsebakke <jorgen.fulsebakke@gmail.com>
2017-11-23 13:10:19 -0500

    Implement rectified linear unit

---
+++ src/School/Unit/RecLin.hs
@@ -0,0 +1,35 @@
+module School.Unit.RecLin
+( recLin ) where
+
+import Numeric.LinearAlgebra ((<>), R, cmap, step)
+import School.Unit.Unit (Unit(..))
+import School.Unit.UnitActivation (UnitActivation(..))
+import School.Unit.UnitGradient (UnitGradient(..))
+import School.Unit.UnitParams (UnitParams(..))
+
+recLin :: Unit R
+recLin = Unit { apply = recLinApply
+              , deriv = recLinDeriv
+              }
+
+recLinApply :: UnitParams R
+            -> UnitActivation R
+            -> UnitActivation R
+recLinApply EmptyParams (BatchActivation inMatrix) =
+  BatchActivation $ cmap (max 0) inMatrix
+recLinApply _ f@(ApplyFail _) = f
+recLinApply _ _ = ApplyFail "Failure when applying RecLin unit" 
+
+recLinDeriv :: UnitParams R
+            -> UnitGradient R
+            -> UnitActivation R
+            -> ( UnitGradient R
+               , UnitParams R)
+recLinDeriv EmptyParams
+            (BatchGradient inGrad)
+            (BatchActivation input)
+  = (BatchGradient grad, EmptyParams)
+    where grad = inGrad <> (step input)
+recLinDeriv EmptyParams f@(GradientFail _) _ = (f, EmptyParams)
+recLinDeriv EmptyParams _ (ApplyFail msg) = (GradientFail msg, EmptyParams)
+recLinDeriv _ _ _ = (GradientFail "Failure when differentiating RecLin unit" , EmptyParams)
