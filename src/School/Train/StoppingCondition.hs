d76bb264d75ce00a35194847c5ec222ced513788
jorgen.fulsebakke <jorgen.fulsebakke@gmail.com>
2017-12-09 22:27:18 -0500

    Simplify StoppingCondition

--- src/School/Train/StoppingCondition.hs
+++ src/School/Train/StoppingCondition.hs
@@ -10,15 +10,17 @@ import School.Train.TrainState (TrainState(..))
 newtype StoppingCondition a =
   StoppingCondition { runCondition :: TrainState a -> Bool }
 
+toCondition :: (TrainState a -> Bool)
+            -> StoppingCondition a
+toCondition runCondition =
+  StoppingCondition { runCondition }
+
 instance Monoid (StoppingCondition a) where
   mappend c1 c2 =
-    StoppingCondition { runCondition = run }
-      where run s = (runCondition c1) s
-                 || (runCondition c2) s
-  mempty = StoppingCondition { runCondition }
-    where runCondition = const False
+    toCondition $ \s -> (runCondition c1) s
+                     || (runCondition c2) s
+  mempty = toCondition $ const False
 
 maxIterations :: Int -> StoppingCondition a
 maxIterations n =
-  let runCondition = \(TrainState { iterationCount }) -> iterationCount >= n
-  in StoppingCondition { runCondition }
+  toCondition $ \s -> iterationCount s >= n
